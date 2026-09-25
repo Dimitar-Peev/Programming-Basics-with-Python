114bd2efbc0dae7095f4edf219f462f5cd6e5f5e
Dimitar-Peev <mctrix87@abv.bg>
2026-09-25 15:53:56 +0300

    Solution - Task 6

---
+++ 01_first_steps_in_coding/02_exercise/06_repainting.py
@@ -0,0 +1,14 @@
+nylon = int(input())
+paint = int(input())
+paint_thinner = int(input())
+hours_workers = int(input())
+
+price_nylon = (nylon + 2) * 1.5
+price_paint = (paint * 1.1) * 14.5
+price_paint_thinner = paint_thinner * 5
+
+sum_materials = price_nylon + price_paint + price_paint_thinner + 0.4
+sum_workers = (sum_materials * 0.3) * hours_workers
+total_sum = sum_materials + sum_workers
+
+print(total_sum)
